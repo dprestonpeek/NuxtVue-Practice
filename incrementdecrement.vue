2958672ae1faf5c3c223acf80387b9cd33d4e329
Preston Peek <36671440+dprestonpeek@users.noreply.github.com>
2025-06-30 16:52:29 -0400

    increment / decrement vue practice

---
+++ incrementdecrement.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  data() {
+    return {
+      msg: 0
+      
+    }
+  },
+
+  methods: {
+    increment() {
+      this.msg = this.msg + 1 
+    },
+    
+    decrement() {
+      if (this.msg > 0) {
+        this.msg = this.msg - 1 
+      }
+    },
+
+    reset() {
+      this.msg = 0
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="increment">Increment</button>
+
+  <button @click="decrement">Decrement</button>
+  <br><br>
+  <button @click="reset">Reset</button>
+</template>
